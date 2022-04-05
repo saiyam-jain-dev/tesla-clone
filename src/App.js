6572f09bd80524fec2d97497968214aaca462404
Saiyam <jimisierra2@gmail.com>
2022-04-05 21:54:34 +0530

    tesla

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
